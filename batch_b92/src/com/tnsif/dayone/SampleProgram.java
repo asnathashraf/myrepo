19d124dc5ca3ee697513fbfad1bf491276bc5370
asnat <asnat@LAPTOP-4NT4TT2L>
2024-06-23 00:22:32 +0530

    commit 2

--- batch_b92/src/com/tnsif/dayone/SampleProgram.java
+++ batch_b92/src/com/tnsif/dayone/SampleProgram.java
@@ -7,6 +7,7 @@ public class SampleProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("hello");
 	}
 
 }
