5c8cfb076b8b49eacad8ec0d0bd584e918c6ac5d
asnat <asnat@LAPTOP-4NT4TT2L>
2024-06-23 17:03:51 +0530

    commit 4

--- batch_b92/src/com/tnsif/dayone/SampleProgram.java
+++ batch_b92/src/com/tnsif/dayone/SampleProgram.java
@@ -7,8 +7,8 @@ public class SampleProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		System.out.println("hello");
-		System.out.print("guys");
+		System.out.print("hello");
+		System.out.println("guys");
 	}
 
 }
