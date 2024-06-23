aef526e21b5542901ae6e8565123ca10b6b6c13c
asnat <asnat@LAPTOP-4NT4TT2L>
2024-06-23 17:33:58 +0530

    commit 5

--- batch_b92/src/com/tnsif/daythree/EncapsulationDemo.java
+++ batch_b92/src/com/tnsif/daythree/EncapsulationDemo.java
@@ -5,5 +5,9 @@ public class EncapsulationDemo {
 	public static void main(String[] args)
 	{
 		OopsConceptDemo obj=new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("asnath");
+		obj.setAge(21);
+		System.out.println(obj);
 	}
 }
