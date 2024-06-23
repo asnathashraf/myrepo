78fb6a41cd342414d2a60360b92ceb0d6d88db4f
asnat <asnat@LAPTOP-4NT4TT2L>
2024-06-23 20:10:03 +0530

    commit 7

--- batch_b92/src/com/tnsif/dayfour/Excecutor.java
+++ batch_b92/src/com/tnsif/dayfour/Excecutor.java
@@ -3,12 +3,10 @@ package com.tnsif.dayfour;
 public class Excecutor {
 
 	public static void main(String[] args) {
-		Base b1=new Base();
-		
+		Base b1=new Base();	
 		b1.methodDefault();
 		b1.methodPublic();
 		b1.methodPrivate();
 		b1.methodProtected();
 	}
-
 }
